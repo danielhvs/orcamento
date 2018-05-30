8de28d05163a7c07387968d98c5907c3a5656505
danielhvs <danielhvs70@gmail.com>
2018-05-30 12:06:43 -0300

    primeiro arquivo inicio parse

--- src/orcamento/core.clj
+++ src/orcamento/core.clj
@@ -1,6 +1,10 @@
 (ns orcamento.core
-  (:require [clojure.edn :as edn])
-)
+  (:require 
+   [clojure.edn :as edn]
+   [clojure.java.io :as io]
+   [clojure.string :as str]
+   [clojure.pprint :refer :all]
+))
 
 (defn dec-prazo [item]
   (assoc item :prazo (dec (:prazo item))))
@@ -17,14 +21,18 @@
       (spit conteudo)))
 
 (defn carrega [arquivo]
-  (-> (str "resources/" arquivo)
-      (slurp)
-      (edn/read-string)))
+  (with-open [r (io/reader (str "resources/" arquivo))]
+    (doall (line-seq r))))
+
+(defn filtra [arquivo]
+  (let [gastos (carrega arquivo)]
+    (filter #(re-matches #"\d\d\.\d\d\.\d\d\d\d.*" %) gastos)))
 
 (defn -main []
   (let [lista [{:prazo 1 :valor 32} {:prazo 7 :valor 8}]]
     (do
-      (println (valor-mensal lista))
-      (println (valor-mensal (proximos lista)))
-      (println lista)
-      (println (proximos lista)))))
+      (pprint (filtra "OUROCARD_PLATINUM_ESTILO_VISA-Dez_17.txt"))
+      (pprint (valor-mensal lista))
+      (pprint (valor-mensal (proximos lista)))
+      (pprint lista)
+      (pprint (proximos lista)))))
